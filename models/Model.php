295e0b828efe5c70001051edb3561243228a5a03
henry <hrobben@robusoft.nl>
2015-06-02 21:10:24 +0200

    Rejected commit: Default (3)

--- models/Model.php
+++ models/Model.php
@@ -7,7 +7,7 @@ class Model
 
     public function __construct()
     {
-        // assert($this->table !== null);   // for debugging alright, but in production better use:  if with exception.
+        // assert($this->table !== null); // for debugging alright, but in production better use:  if with exception.
         assert(isset($this->table));
         if (!isset($this->table)) {
             throw new Exception('Records must be set before this is called');
